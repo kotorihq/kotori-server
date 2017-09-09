2b8435ed00094f06bc9b5422d2ee658809aaa277
frohikey <frohikey@gmail.com>
2017-09-09 17:08:08 +0200

    Some comments

--- KotoriServer/Controllers/BaseController.cs
+++ KotoriServer/Controllers/BaseController.cs
@@ -3,15 +3,25 @@ using Microsoft.Extensions.Configuration;
 
 namespace KotoriServer.Controllers
 {
+    /// <summary>
+    /// Base controller.
+    /// </summary>
     public class BaseController : Controller
     {
         IConfiguration _config;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:KotoriServer.Controllers.BaseController"/> class.
+        /// </summary>
+        /// <param name="config">Config.</param>
         public BaseController(IConfiguration config)
         {
             _config = config;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:KotoriServer.Controllers.BaseController"/> class.
+        /// </summary>
         public BaseController()
         {
         }
