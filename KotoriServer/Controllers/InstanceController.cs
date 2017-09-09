885fb9268a9eea9cc5bea540cdc9983d828ad08a
frohikey <frohikey@gmail.com>
2017-09-09 17:17:55 +0200

    Add instance controller

--- KotoriServer/Controllers/InstanceController.cs
+++ KotoriServer/Controllers/InstanceController.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace KotoriServer.Controllers
 {
+    /// <summary>
+    /// Instance controller.
+    /// </summary>
+    [Route("api/instance")]
     public class InstanceController : BaseController
     {
-        public InstanceController()
-        {
-        }
+		[HttpGet]
+		[ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(void), 401)]
+		[ProducesResponseType(typeof(void), 500)]
+		public string Get()
+		{
+            return "???";
+		}
     }
 }
