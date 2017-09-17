ac85be2bfc9c943e47b82b590b50206cedc3f3ac
frohikey <frohikey@gmail.com>
2017-09-17 20:43:37 +0200

    Clean ups

--- KotoriServer/Helpers/Enums.cs
+++ KotoriServer/Helpers/Enums.cs
@@ -5,19 +5,6 @@
     /// </summary>
     public sealed class Enums
     {
-        /// <summary>
-        /// Claim types.
-        /// </summary>
-        public enum ClaimType
-        {
-            /// <summary>
-            /// The master.
-            /// </summary>
-            Master = 0,
-            /// <summary>
-            /// The project.
-            /// </summary>
-            Project = 1
-        }
+        
     }
 }
